ed58d5e942a6e2c2628d821ad84c0e4da9948eca
= <yysi8771@gmail.com>
2022-02-26 19:49:23 +0800

    회원가입 중복 처리

--- src/main/java/com/lms/lms/member/controller/MemberController.java
+++ src/main/java/com/lms/lms/member/controller/MemberController.java
@@ -1,18 +1,15 @@
 package com.lms.lms.member.controller;
 
-import com.lms.lms.member.entity.Member;
 import com.lms.lms.member.model.MemberInput;
-import com.lms.lms.member.repository.MemberRepsitory;
 import com.lms.lms.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.time.LocalDateTime;
 
 @RequiredArgsConstructor
 @Controller
@@ -32,11 +29,13 @@ public class MemberController {
     }
 
     @RequestMapping(value = "member/register", method = RequestMethod.POST)
-    public String registerSubmit(HttpServletRequest request, HttpServletResponse response, MemberInput parameter){
+    public String registerSubmit(Model model, HttpServletRequest request, MemberInput parameter){
 
 
 
         boolean result = memberService.register(parameter);
+        model.addAttribute("result",result);
+
 
 
         return "member/register_complete";
